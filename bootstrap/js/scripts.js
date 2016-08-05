1bf17705a7c1d73c570a0d9e0a8dc654e454721c
paparastas <paparastas1@gmail.com>
2016-08-05 20:55:06 +0300

    The best sidebar and comments for current moment

--- bootstrap/js/scripts.js
+++ bootstrap/js/scripts.js
@@ -1,10 +1,64 @@
-$(document).ready(function(){
-	$('.spoiler-text').hide();
-	$('.spoiler').click(function(){
-		$(this).toggleClass("folded").toggleClass("unfolded").next().slideToggle();
-	});
+var status_text = null; // Status text var
+var status_status = 0; // 0 = non-edit mode | 1 = edit mode
 
-	$('.settings').click(function() {
-		$('.sidebar-nav').toggle("slide");
-	});
+function hide_textarea() {
+    $('#status-input').val('');
+    $('#status-input').toggle();
+    $('#status-confirm').toggle();
+    $('#status-edit').toggle();
+    $('#status-text').toggle();
+    status_status = 0;
+}
+
+
+$(document).ready(function() {
+    status_text = "Default status. Hello world!"
+    $('#status-text').text(status_text);
+    // Spoiler scripts
+    $('.spoiler').click(function() {
+        $(this).toggleClass("folded").toggleClass("unfolded").next().slideToggle();
+    });
+
+    // Sidebar-slider
+    $('.settings').click(function() {
+        $('.sidebar-nav').toggle("slide");
+    });
+
+    // Status: click edit button
+    $('#status-edit').click(function() {
+        $(this).toggle();
+        $('#status-text').toggle();
+        $('#status-confirm').toggle();
+        $('#status-input').toggle();        
+        $('#status-input').val(status_text);
+        status_status = 1;
+    });
+
+    // Status: click confirm buttom
+    $('#status-confirm').click(function() {
+        status_text = $('#status-input').val();
+        $('#status-text').text(status_text);
+        $(this).toggle();
+        $('#status-text').toggle();
+        $('#status-edit').toggle();
+        $('#status-input').toggle();
+        status_status = 0;
+    });
+
+    // Status: click delete button. Smart - when no text will work fine
+    $('#status-delete').click(function() {         
+        if (status_status == 1) { 
+            hide_textarea();         
+        } else {
+            if (status_text) {$('#modal-1').modal('show');}
+        }         
+    });
+
+    // Modal: delete confirmation button
+    $('#btn-delete-btn').click(function() {
+        status_text = null;
+        $('#modal-1').modal('hide');
+        $('#status-text').text('');
+        $('#status-input').val('');        
+    });
 });
